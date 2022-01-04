df778ee4d525458a33d77ce51a5853a8b03824a3
Evgeney Zimin <evgeneyz@vg-gpu-01.lab.graphicon.ru>
2022-01-04 20:23:48 +0300

    Change intefrace

--- run.py
+++ run.py
@@ -11,14 +11,12 @@ with open("/model/run.sh", 'w') as f:
     f.write("cp pyflow*.so ..\n")
     f.write("chmod -R 0777 /model\n")
 
-    f.write("mkdir /model/result\n")
-
     videos = os.listdir("/dataset")
     for video in videos:
-        f.write(f"mkdir /model/result/{video}\n")
-        f.write(f"python3 /model/iSeeBetterTest.py -o /model/result/{video} -s 4 -c --data_dir /dataset/{video} --future_frame False -u -m /model/weights/RBPN_4x.pth --gpus 1 --threads 1\n")
+        f.write(f"mkdir /results/{video}\n")
+        f.write(f"python3 /model/iSeeBetterTest.py -o /results/{video} -s 4 -c --data_dir /dataset/{video} --future_frame False -u -m /model/weights/RBPN_4x.pth --gpus 1 --threads 1\n")
 
-    f.write("chmod -R 0777 /model/result\n")
+    f.write("chmod -R 0777 /results\n")
 
 os.system("chmod 0777 /model -R")
 os.system("chmod 0777 /model/run.sh")
